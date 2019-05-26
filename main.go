93db2348c3ab29416387bea4c3eae88304d63585
nanxuezhi <1562985905@coolpad.com>
2019-05-26 21:13:15 +0800

    format

--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
-	common "github.com/ielepro/k8s-client-common/common"
+	"github.com/ielepro/k8s-client-common/common"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
-	common.InitClient()
-	fmt.Println("11")
+	var (
+		clientSet *kubernetes.Clientset
+		podsList  *coreV1.PodList
+		err       error
+	)
+
+	// 初始化k8s客户端
+	if clientSet, err = common.InitClient(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 获取default命名空间下的所有POD
+	if podsList, err = clientSet.CoreV1().Pods("default").List(metaV1.ListOptions{}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(*podsList)
+	return
 }
